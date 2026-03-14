16fd44fcd784fdffda9d636b5ab1e30919e0fa6d
Laurence Trippen <laurence.trippen@gmail.com>
2026-03-14 10:02:00 +0400

    initial commit

---
+++ bin/git_finder.dart
@@ -0,0 +1,5 @@
+import 'package:git_finder/git_finder.dart' as git_finder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_finder.calculate()}!');
+}
---
+++ test/git_finder_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_finder/git_finder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
